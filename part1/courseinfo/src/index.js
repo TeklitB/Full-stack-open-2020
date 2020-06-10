fb05ab5a958f13517e76450adaf03379004a25dd
teklit <teklit@student.chalmers.se>
2020-06-10 11:44:44 +0200

    solution to courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -51,30 +51,6 @@ const Total = (props) => {
   )
 }
 
-const Statistics = (props) => {
-  let feedbackSum = props.good + props.neutral + props.bad;
-  let feedback = props.good + props.neutral * 0 + props.bad * -1;
-
-  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
-    return (
-      <div>
-        <p>No feedback given</p>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h2>statistics</h2>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {feedbackSum}</p>
-      <p>average {feedback / feedbackSum}</p>
-      <p>positive {props.good / feedbackSum}</p>
-    </div>
-  )
-}
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -93,25 +69,12 @@ const App = () => {
       }
     ]
   }
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
 
   return (
     <div>
       <Header course={course} />
       <Content course={course} />
       <Total course={course} />
-      <div>
-        <h2>give feedback</h2>
-        <button onClick={() => setGood(good + 1)}> good </button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
-      </div>
-      <div>
-        <Statistics good={good} neutral={neutral} bad={bad} />
-      </div>
     </div>
   )
 }
