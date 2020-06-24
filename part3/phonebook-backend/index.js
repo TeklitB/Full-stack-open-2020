b740cb900a4ed338f9afd064a8c0bf8d3d2ce325
teklit <teklit@student.chalmers.se>
2020-06-24 15:07:16 +0200

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -119,6 +119,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const port = 3001
-app.listen(port)
-console.log(`Server is running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
